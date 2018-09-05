261f70daf3c39f7f59c6c7b75fc0fbcd3f9884b9
ADraWone <avelez90@misena.edu.co>
2018-09-05 15:50:29 -0500

    Siguiente

--- HuellaCarbonoWeb/Models/Encuesta.js
+++ HuellaCarbonoWeb/Models/Encuesta.js
@@ -3,7 +3,46 @@
     //var personas = function ($scope, nombre del servicio) {
 	var encuesta = function ($scope) {
 
+		$("#botonPasar").click(function() {
+			var enlace = $(this).attr("enlace");
+			var posX = $(enlace).position().left;
+
+			$("html,body").stop(true,true).animate({
+				scrollLeft:posX+"px"
+			},1000, "easeOutExpo")
+		})
+
+		//Transporte
+		$("#ant1").click(function () {
+			var enlace = $(this).attr("enlace");
+			var posX = $(enlace).position().left;
+
+			$("html,body").stop(true, true).animate({
+				scrollLeft: posX + "px"
+			}, 1000, "easeOutExpo")
+		})
+
+		$("#sig1").click(function () {
+			var enlace = $(this).attr("enlace");
+			var posX = $(enlace).position().left;
+
+			$("html,body").stop(true, true).animate({
+				scrollLeft: posX + "px"
+			}, 1000, "easeOutExpo")
+		})
+
+		//Vuelos
+		$("#ant2").click(function () {
+			var enlace = $(this).attr("enlace");
+			var posX = $(enlace).position().left;
+
+			$("html,body").stop(true, true).animate({
+				scrollLeft: posX + "px"
+			}, 1000, "easeOutExpo")
+		})
+
 	}
+	
 
 
     //inyectar las dependencias que se esta usando
